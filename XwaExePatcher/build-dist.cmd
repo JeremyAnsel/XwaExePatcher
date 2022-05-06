648a837bc81693052dd9013af92a626c7c2688cf
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2022-05-06 21:39:45 +0200

    Update dist

--- XwaExePatcher/build-dist.cmd
+++ XwaExePatcher/build-dist.cmd
@@ -12,6 +12,7 @@ xcopy /s /d "%%~a" dist\lib\
 For %%a in (
 "XwaExePatcherConsole\bin\Release\net48\*.dll"
 "XwaExePatcherConsole\bin\Release\net48\*.exe"
+"XwaExePatcherConsole\bin\Release\net48\*.config"
 ) do (
 xcopy /s /d "%%~a" dist\Console\
 )
@@ -19,6 +20,7 @@ xcopy /s /d "%%~a" dist\Console\
 For %%a in (
 "XwaExePatcherWindow\bin\Release\net48\*.dll"
 "XwaExePatcherWindow\bin\Release\net48\*.exe"
+"XwaExePatcherWindow\bin\Release\net48\*.config"
 "XwaExePatcherWindow\bin\Release\net48\*.xml"
 "XwaExePatcherWindow\bin\Release\net48\*.xsd"
 "XwaExePatcherWindow\bin\Release\net48\*.txt"
